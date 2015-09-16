4e6324b4bd347a9e3c25f0af329485a135eb380a
Alex Hockey <alex.hockey@metaswitch.com>
2015-09-16 21:26:06 +0100

    Solaris build fixes

--- ext/lz4ruby/extconf.rb
+++ ext/lz4ruby/extconf.rb
@@ -1,6 +1,13 @@
 require 'mkmf'
+require 'rbconfig'
 
 $CFLAGS += " -Wall "
+
+# On some Solaris systems ruby reports linker flags that are not supported by
+# the linker. Fix this up.
+if RbConfig::CONFIG['host_os'] =~ /solaris|sunos/i
+  $LDFLAGS.gsub!('-Wl,-E', '-Wl,-dy')
+end
 
 create_makefile('lz4ruby')
 
